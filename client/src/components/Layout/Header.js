ac5e2b5e1fca33a16161e065f8abf8cdcb43067e
ayushsharma2004 <ayush.s.sharma04@gmail.com>
2023-10-05 20:54:54 +0530

    Made Most of the pages

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { GiShoppingBag } from 'react-icons/gi';
+// import { GiShoppingBag } from 'react-icons/gi';
 
 const Header = () => {
   return (
@@ -20,7 +20,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              <GiShoppingBag /> Ecommerce Website
+              🛍️ Ecommerce Website
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import Layout from '../components/Layout/Layout';
+import Layout from './../components/Layout/Layout';
 
 const Policy = () => {
   return (
     <Layout>
-      <h1>Private Policy Page</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: '100%' }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
     </Layout>
   );
 };
